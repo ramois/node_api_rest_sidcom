0f85631dac4b2ecc1b9b5e239e275216d1732461
Sussy Ajata <raul.chis33@gmail.com>
2024-07-25 14:13:17 -0400

    se cambio los campos id_operador opcional y estado

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -8,8 +8,16 @@ const userSchema =new mongoose.Schema(
         ci: String,
         celular: Number,
         rol: Number,
-        id_operador: Number,
-        estado: Number
+        //id_operador: Number,
+        id_operador: {
+            type: Number,
+            default: null // Hacerlo opcional con un valor predeterminado de null
+        },
+        //estado: Number
+        estado: {
+            type: Number,
+            default: 0 // Valor predeterminado para el estado
+        }
     }
 )
 module.exports=mongoose.model('User', userSchema)
